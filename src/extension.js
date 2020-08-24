7d1be1e7c8a34a7af2d5952db9680daf60aed8b4
Mike Panitz <michpa@hotmail.com>
2020-08-23 18:38:29 -0700

    Published extension for the world to use :)

--- src/extension.js
+++ src/extension.js
@@ -1,4 +1,22 @@
+// MultiSearch: Enable an array of search terms, keyboard shortcuts to search for them,
+// and the ability to load the terms in from a JSON file
+//
+// Copyright (C) 2020 Mike Panitz
+
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import * as vscode from "vscode";
+
 import { TextEditorEdit, TextEditor, Selection } from "vscode";
 import { resolveCliPathFromVSCodeExecutablePath } from "vscode-test";
 import { isUndefined } from "util";
